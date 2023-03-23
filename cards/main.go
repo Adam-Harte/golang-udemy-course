29f93d34715bbf9a603bee8d92086fe60a350454
Adam Harte <adamh@CA-MBP-458.local>
2023-03-23 23:12:49 +0000

    custom types

--- cards/main.go
+++ cards/main.go
@@ -3,14 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace of diamonds", newCard()}
+	cards := deck{"Ace of diamonds", newCard()}
 	cards = append(cards, "Six of spades")
 
 	for i, card := range cards {
 		fmt.Println(i, card)
 	}
-
-	fmt.Println(cards)
 }
 
 func newCard() string {
