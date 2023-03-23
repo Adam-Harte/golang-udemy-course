dbd0abf9079d0498593701d0ce3628369c29fadf
Adam Harte <adamh@CA-MBP-458.local>
2023-03-23 22:47:02 +0000

    function definitions

--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of spades"
-	card := "Ace of spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
